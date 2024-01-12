3d190630830362f695e2249efc6e954d41d713dd
Marcelo Heredia <marcelo.heredia@acad.pucrs.br>
2024-01-11 23:11:19 -0300

    initial project with dotnet8 and winuiex

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using SaveMe.Properties;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace SaveMe
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            ConfigureTitleBar();
+
+
+            //ExtendsContentIntoTitleBar = true;
+            //SetTitleBar(AppTitleBar);
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            myButton.Content = "Clicked";
+        }
+
+        private void ConfigureTitleBar()
+        {
+            if (!AppWindowTitleBar.IsCustomizationSupported()) return;
+
+            Title = Resources.AppTitle;
+    
+            AppWindow.SetIcon(Resources.AppIcon);
+        }
+    }
+}
